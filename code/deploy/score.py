61101d8533773ff2199341ced586a59d685f8082
lordlinus <sunilsattiraju@gmail.com>
2021-11-30 14:40:56 +0800

    update pipeline

--- code/deploy/score.py
+++ code/deploy/score.py
@@ -8,6 +8,7 @@ from inference_schema.schema_decorators import input_schema, output_schema
 from azureml.core.model import Model
 
 # This is from develop branch - update 1
+# update from master branch
 
 # The init() method is called once, when the web service starts up.
 #
